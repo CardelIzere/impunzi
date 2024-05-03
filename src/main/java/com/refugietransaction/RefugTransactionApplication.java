c5583cc43fabe856ac6e121c2cf320f13d9771e0
IgirubElam <igirubuntuelam@gmail.com>
2024-05-03 14:32:39 +0200

    commit

--- src/main/java/com/refugietransaction/RefugTransactionApplication.java
+++ src/main/java/com/refugietransaction/RefugTransactionApplication.java
@@ -3,27 +3,16 @@ package com.refugietransaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.refugietransaction.services.impl.ProductTypeServiceImpl;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class RefugTransactionApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(RefugTransactionApplication.class, args);
 		
-		//Retrieve the ProductTypeService bean from the application context
-		ProductTypeServiceImpl myDataService = context.getBean(ProductTypeServiceImpl.class);
+		SpringApplication.run(RefugTransactionApplication.class, args);
 		
-		//Call loadData method
-		myDataService.loadData();
 		
-		//You can continue with other application logic here if needed
-		
-		//Close the application context
-		context.close();
 	}
 
 }

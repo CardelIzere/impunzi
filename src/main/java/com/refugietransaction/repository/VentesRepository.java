c7213361dd681cd9bb835628e2408e49f74e2377
IgirubElam <igirubuntuelam@gmail.com>
2024-05-15 16:12:53 +0200

    Commit

--- src/main/java/com/refugietransaction/repository/VentesRepository.java
+++ src/main/java/com/refugietransaction/repository/VentesRepository.java
@@ -42,7 +42,7 @@ public interface VentesRepository extends JpaRepository<Ventes, Long> {
 	@Query("select v from Ventes v join Menage m on v.menage.id = m.id where TO_CHAR(v.dateVente, '%Y-%m-%d') BETWEEN :startDate AND :endDate AND v.supplier.id = :idSupplier AND v.venteStatusEnum = 'PAID' order by v.id desc")
 	Page<Ventes> findByStartDateAndEndDateAndSupplierId(Date startDate, Date endDate, Long idSupplier, Pageable pageable);
 	
-	@Query("SELECT SUM(lv.prixUnitaire * lv.quantite) FROM Ventes v join Menage m on v.menage.id = m.id LEFT JOIN v.ligneVentes lv where TO_DATE(v.dateVente, '%Y-%m-%d') BETWEEN :startDate AND :endDate AND v.supplier.id = :idSupplier AND v.venteStatusEnum = 'PAID'")
+	@Query("SELECT SUM(lv.prixUnitaire * lv.quantite) FROM Ventes v join Menage m on v.menage.id = m.id LEFT JOIN v.ligneVentes lv where TO_CHAR(v.dateVente, '%Y-%m-%d') BETWEEN :startDate AND :endDate AND v.supplier.id = :idSupplier AND v.venteStatusEnum = 'PAID'")
 	BigDecimal sumByStartDateAndEndDateAndSupplierId(Date startDate , Date endDate , Long idSupplier);
 	
 	@Query("select v from Ventes v join Menage m on v.menage.id = m.id where TO_CHAR(v.dateVente, '%Y-%m-%d') BETWEEN :startDate AND :endDate AND v.supplier.id = :idSupplier AND v.venteStatusEnum = 'PAID' And UPPER(m.personneContact) like CONCAT('%',UPPER(:search),'%') order by v.id desc")

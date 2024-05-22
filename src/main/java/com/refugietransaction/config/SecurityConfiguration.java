0803ddeb2a4c5e8141954a1d5d901cc89d6d673d
IgirubElam <igirubuntuelam@gmail.com>
2024-05-22 13:46:05 +0200

    commit

--- src/main/java/com/refugietransaction/config/SecurityConfiguration.java
+++ src/main/java/com/refugietransaction/config/SecurityConfiguration.java
@@ -20,7 +20,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.session.SessionManagementFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -31,53 +30,64 @@ import java.util.Collections;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class SecurityConfiguration   {
+public class SecurityConfiguration {
+	
+	@Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth->auth
                         .requestMatchers(
-                                "/auth/login",
-                                "//authenticate",
-                                "//superadmins/create",
-                                "//admins/create",
-                                "//magasiniers/create",
-                                "/api/access/",
-                                "/h2-console/",
-                                // resources for swagger to work properly
-                                "/v2/api-docs",
-                                "/v3/api-docs",
-                                "/v3/api-docs/",
-                                "/swagger-resources",
-                                "/swagger-resources/",
-                                "/configuration/ui",
-                                "/configuration/security",
-                                "/swagger-ui/",
-                                "/webjars/",
-                                "/swagger-ui.html"
+                        		 "/auth/login",
+                                 "//authenticate",
+                                 "//superadmins/create",
+                                 "//admins/create",
+                                 "//magasiniers/create",
+                                 "/api/access/",
+                                 "/h2-console/",
+                                 // resources for swagger to work properly
+                                 "/v2/api-docs",
+                                 "/v3/api-docs",
+                                 "/v3/api-docs/",
+                                 "/swagger-resources",
+                                 "/swagger-resources/",
+                                 "/configuration/ui",
+                                 "/configuration/security",
+                                 "/swagger-ui/",
+                                 "/webjars/",
+                                 "/swagger-ui.html"
                         )
                         .permitAll()
-                        .anyRequest().authenticated()
-                )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        .anyRequest().authenticated())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                //.authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(corsFilter(), SessionManagementFilter.class)
+                .build();
+    }
 
-        return http.build();
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Don't do this in production, use a proper list  of allowed origins
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        // some comment here
+        return new CorsFilter(source);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-//        return config.getAuthenticationManager();
-//    }
-    
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-    	return config.getAuthenticationManager();
+        return config.getAuthenticationManager();
     }
-
 }
